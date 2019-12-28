945264a7664c81997b2ba54d60d9f1704c94cc49
teodorro <edem.teo@gmail.com>
2019-12-28 22:51:50 +0300

    skeleton

---
+++ CmpScanModule/Views/CmpScanCtrl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CmpScanModule.Views
+{
+    /// <summary>
+    /// Interaction logic for CmpScanCtrl.xaml
+    /// </summary>
+    public partial class CmpScanCtrl : UserControl
+    {
+        public CmpScanCtrl()
+        {
+            InitializeComponent();
+        }
+    }
+}
